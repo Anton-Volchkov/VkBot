010e8f0ec282b0c755945026b7fa1def15075f7f
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-04-09 13:27:26 +0300

    UpdateDatabase

---
+++ VkBot/Data/Migrations/20190409102641_UserMemory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace VkBot.Data.Migrations
+{
+    public partial class UserMemory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Memory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    UserID = table.Column<long>(nullable: false),
+                    Memory = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memory", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Memory");
+        }
+    }
+}
