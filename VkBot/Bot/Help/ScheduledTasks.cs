391e14cbc7637824ed4f34ed37c08e2e4e4ad0fe
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-05-08 08:59:43 +0300

    Изменил время рассылки

--- VkBot/Bot/Help/ScheduledTasks.cs
+++ VkBot/Bot/Help/ScheduledTasks.cs
@@ -53,7 +53,7 @@ namespace VkBot.Bot.Help
         private void InitJobs()
         {
             RecurringJob.AddOrUpdate<ScheduledTask>("SendWeather", x => x.SendWeather(),
-                                                    "00 6 * * *", TimeZoneInfo.Local);
+                                                    "40 9 * * *", TimeZoneInfo.Local);
         }
     }
 }
