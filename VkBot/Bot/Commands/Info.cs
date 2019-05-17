7572d772388859e7edb85f0839122ec37dae0391
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-05-17 22:38:34 +0300

    Исправить баг с командой !бот инфо инфо

--- VkBot/Bot/Commands/Info.cs
+++ VkBot/Bot/Commands/Info.cs
@@ -10,7 +10,7 @@ namespace VkBot.Bot.Commands
     public class Info : IInfo
     {
         public string[] Alliases { get; set; } = { "инфо", "информация" };
-        public string Description { get; set; } = "Команда !Бот инфо информацию о команде и пример ее использования." +
+        public string Description { get; set; } = "Команда !Бот инфо возвращает информацию о команде и пример ее использования." +
                                                   "\nПример: !Бот инфо Звонок ";
 
         private readonly IBotCommand[] Commands;
@@ -24,6 +24,11 @@ namespace VkBot.Bot.Commands
         public async Task<string> Execute(Message msg)
         {
             var split = msg.Text.Split(' ', 2); // [команда, параметры]
+
+            if (split[1].Trim().ToLower() == "инфо" || split[1].Trim().ToLower() == "информация")
+            {
+                return this.Description;
+            }
 
             foreach (var command in Commands)
             {
