c2223a0f56b9925218be8790edf6430314964fba
Anton Volchkov <anton.volchkov.2001@gmail.com>
2020-06-07 18:31:36 +0300

    Вынести некоторые расширения в другой проект

---
+++ VkBot.Extensions/Extensions/CollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VkBot.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static List<T> TakeRandomElements<T>(this List<T> collection, int count)
+        {
+            var randomNumbers = new int[count];
+            var outputArray = new List<T>();
+
+            var rnd = new Random(new DateTime().Millisecond + Guid.NewGuid().ToByteArray().Sum(x => x));
+
+            for(int i = 0; i < count; i++)
+            {
+                while(true)
+                {
+                    var number = rnd.Next(0, collection.Count);
+
+                    if(!randomNumbers.Contains(number))
+                    {
+                        randomNumbers[i] = number;
+                        break;
+                    }
+                }
+            }
+
+            foreach(var randomNumber in randomNumbers)
+            {
+                outputArray.Add(collection[randomNumber]);
+            }
+
+            return outputArray;
+        }
+    }
+}
