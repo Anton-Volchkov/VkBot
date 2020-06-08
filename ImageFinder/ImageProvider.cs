0165fac96383f3647a642583b7673785e06da793
Anton Volchkov <anton.volchkov.2001@gmail.com>
2020-06-08 14:06:24 +0300

    Добавить локер

--- ImageFinder/ImageProvider.cs
+++ ImageFinder/ImageProvider.cs
@@ -17,6 +17,7 @@ namespace ImageFinder
     public class ImageProvider
     {
         public string PathToChromeDriver{ get; set; }
+        public object Locker { get; set; } = new object();
 
         public ImageProvider(string pathToChromeDriver)
         {
@@ -30,29 +31,30 @@ namespace ImageFinder
             options.AddArguments("-disable-gpu");
             options.AddArguments("--headless");
 
-            IWebDriver driver = new ChromeDriver(PathToChromeDriver, options);
-
-            driver.Url = $"https://yandex.by/images/search?text={category.Trim().Replace(" ","+")}";
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
-
-            var elements = driver.FindElements(By.XPath("//div[contains(@class, 'serp-item__preview')]/a/img"));
-
-           
-            var listUrl = new List<string>();
-
-           
-
-            foreach (var iElement in elements.ToList().TakeRandomElements(3))
+            lock(Locker)
             {
-                listUrl.Add(iElement.GetAttribute("src"));
+                IWebDriver driver = new ChromeDriver(PathToChromeDriver, options);
+
+                driver.Url = $"https://yandex.by/images/search?text={category.Trim().Replace(" ","+")}";
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+
+                var elements = driver.FindElements(By.XPath("//div[contains(@class, 'serp-item__preview')]/a/img"));
+
+           
+                var listUrl = new List<string>();
+
+           
+
+                foreach (var iElement in elements.ToList().TakeRandomElements(3))
+                {
+                    listUrl.Add(iElement.GetAttribute("src"));
+                }
+
+                driver.Quit();
+                return listUrl;
             }
 
-            driver.Quit();
-
-
-            return listUrl;
-           
         }
     }
 }
