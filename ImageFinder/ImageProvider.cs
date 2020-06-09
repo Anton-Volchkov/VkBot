ff17ebb5a1e39d54ddb6b159f2964bacaef585bd
Anton Volchkov <anton.volchkov.2001@gmail.com>
2020-06-09 23:13:08 +0300

    Небольшое код ревью

--- ImageFinder/ImageProvider.cs
+++ ImageFinder/ImageProvider.cs
@@ -1,22 +1,16 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
-using Flurl;
-using Flurl.Http;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using VkBot.Extensions;
 using VkBot.Proxy.Logic;
 
-
 namespace ImageFinder
 {
-    public class ImageProvider 
+    public class ImageProvider
     {
-        public string PathToChromeDriver{ get; set; }
+        public string PathToChromeDriver { get; set; }
         public object Locker { get; set; } = new object();
         private readonly ChromeOptions _options = new ChromeOptions();
         private readonly ProxyProvider _proxyProvider;
@@ -25,17 +19,17 @@ namespace ImageFinder
         {
             PathToChromeDriver = pathToChromeDriver;
             _proxyProvider = proxyProvider;
-            
+
             _options.AddArguments("--no-sandbox");
             _options.AddArguments("-disable-gpu");
             _options.AddArguments("--headless");
-       
         }
+
         public async Task<List<string>> GetImagesUrl(string category)
         {
             var proxyAddress = await _proxyProvider.GetRandomProxy();
-          
-            lock (Locker)
+
+            lock(Locker)
             {
                 if(!string.IsNullOrWhiteSpace(proxyAddress))
                 {
@@ -47,7 +41,7 @@ namespace ImageFinder
                     _options.Proxy = proxy;
                 }
 
-                using IWebDriver browser = new ChromeDriver(PathToChromeDriver,_options);
+                using IWebDriver browser = new ChromeDriver(PathToChromeDriver, _options);
 
 
                 browser.Url = $"https://yandex.by/images/search?text={category.Trim().Replace(" ", "+")}";
@@ -58,18 +52,13 @@ namespace ImageFinder
 
                 var listUrl = new List<string>();
 
-                foreach (var iElement in elements.ToList().TakeRandomElements(3))
-                {
+                foreach(var iElement in elements.TakeRandomElements(3))
                     listUrl.Add(iElement.GetAttribute("src"));
-                }
 
                 browser.Quit();
-                
+
                 return listUrl;
             }
-
         }
-        
-       
     }
 }
--- VkBot.Extensions/Extensions/CollectionExtensions.cs
+++ VkBot.Extensions/Extensions/CollectionExtensions.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace VkBot.Extensions
 {
     public static class CollectionExtensions
     {
-        public static List<T> TakeRandomElements<T>(this List<T> collection, int count)
+        public static IEnumerable<T> TakeRandomElements<T>(this IEnumerable<T> collection, int count)
         {
-            if(collection.Count <= count)
+            if(collection.Count() <= count)
             {
                 return collection;
             }
@@ -19,24 +18,18 @@ namespace VkBot.Extensions
 
             var rnd = new Random(new DateTime().Millisecond + Guid.NewGuid().ToByteArray().Sum(x => x));
 
-            for(int i = 0; i < count; i++)
-            {
+            for(var i = 0; i < count; i++)
                 while(true)
                 {
-                    var number = rnd.Next(0, collection.Count);
+                    var number = rnd.Next(0, collection.Count());
 
                     if(!randomNumbers.Contains(number))
                     {
                         randomNumbers[i] = number;
+                        outputArray.Add(collection.ElementAt(number));
                         break;
                     }
                 }
-            }
-
-            foreach(var randomNumber in randomNumbers)
-            {
-                outputArray.Add(collection[randomNumber]);
-            }
 
             return outputArray;
         }
