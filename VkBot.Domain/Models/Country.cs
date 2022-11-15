7f4f560534f17a5b32f2b488386c3b721a3ff5ae
Anton Volchkov <anton.volchkov.2001@gmail.com>
2022-11-15 14:09:22 +0100

    cleanup

--- VkBot.Domain/Models/Country.cs
+++ VkBot.Domain/Models/Country.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace VkBot.Domain.Models
+﻿namespace VkBot.Domain.Models
 {
     public class Country
     {
--- VkBot/Bot/Commands/Help.cs
+++ VkBot/Bot/Commands/Help.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using VkBot.Data.Abstractions;
 using VkNet.Model;
 
